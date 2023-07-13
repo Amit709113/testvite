ca808dea19da18dff240820cd9f796eef281faef
Amit Kumar Sahu <sahuamitkumr7091@gmail.com>
2023-07-13 09:15:46 +0530

    remove unwanted para and heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-// import src1 from "https://picsum.photos/200/300";
+
 import Testimonial from './components/Testimonial';
 import data from './data/topperData.json';
 import img1 from './assets/preity.jpg';
@@ -10,10 +10,7 @@ function App() {
 
   return (
     <>
-    <h1> Alumni data</h1>
-      <p>deploying static page on github pages</p>
       <Testimonial testimonialData={data} />
-      <img src={img1} alt="my name"  style={{width:'100px', height:'100px'}}/>
     </>
   )
 }
