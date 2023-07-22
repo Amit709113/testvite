40c33e49a443157162c62f7d66aa0ed920bf5112
Amit Kumar Sahu <sahuamitkumr7091@gmail.com>
2023-07-22 16:25:02 +0530

    about page is ready without new app comp.

---
+++ src/components/ContentPageHeading.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import './ContentPageHeading.css'
+
+import AboutMain from './AboutMain';
+
+
+const ContentPageHeading = ({obj}) => {
+
+  const {bgimgsrc,heading,...newObj}=obj;
+  
+  const histroyBgStyle={
+    backgroundImage: `url(${bgimgsrc})`,
+    backgroundRepeat:' no-repeat',
+    backgroundPosition: `0% 70%`,
+    backgroundSize: 'cover',
+    padding:'60px 0' ,
+    width:'100%',
+
+
+    
+  }
+
+  return (
+    <>
+      <div id="section-bradcrum"  style={histroyBgStyle} className='historybg'  >
+        <div className="containera">
+          <div className="rowa">
+            <div className="columna">
+              <h1 className="h1-brandcrum">
+                {heading}
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <AboutMain  data={newObj}/>
+
+    </>
+  )
+}
+
+export default ContentPageHeading
